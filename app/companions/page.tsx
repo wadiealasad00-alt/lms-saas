367438c66ce65d8c74c0c54b3e928002c84a46c7
Your Name <your_email@example.com>
2025-11-25 21:39:26 +0300

    first commit

---
+++ app/companions/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>CompanionsLibrary</div>
+  )
+}
+
+export default page
---
+++ constants/index.ts
@@ -0,0 +1,73 @@
+export const subjects = [
+  "maths",
+  "language",
+  "science",
+  "history",
+  "coding",
+  "economics",
+];
+
+export const subjectsColors = {
+  science: "#E5D0FF",
+  maths: "#FFDA6E",
+  language: "#BDE7FF",
+  coding: "#FFC8E4",
+  history: "#FFECC8",
+  economics: "#C8FFDF",
+};
+
+export const voices = {
+  male: { casual: "2BJW5coyhAzSr8STdHbE", formal: "c6SfcYrb2t09NHXiT80T" },
+  female: { casual: "ZIlrSGI4jZqobxRKprJz", formal: "sarah" },
+};
+
+export const recentSessions = [
+  {
+    id: "1",
+    subject: "science",
+    name: "Dr. Nova Sparks",
+    topic: "How the Brain Remembers & Learns",
+    duration: 45,
+    color: "#E5D0FF",
+  },
+  {
+    id: "2",
+    subject: "maths",
+    name: "Professor Sigma",
+    topic: "The Power of Calculus in Real Life",
+    duration: 30,
+    color: "#FFDA6E",
+  },
+  {
+    id: "3",
+    subject: "language",
+    name: "Lexa Wordsmith",
+    topic: "Crafting Powerful Stories",
+    duration: 30,
+    color: "#BDE7FF",
+  },
+  {
+    id: "4",
+    subject: "coding",
+    name: "Byte Master Orion",
+    topic: "Logic that Powers All Software",
+    duration: 45,
+    color: "#FFC8E4",
+  },
+  {
+    id: "5",
+    subject: "history",
+    name: "Chrono the Time Archivist",
+    topic: "How Conflicts Shape Societies",
+    duration: 15,
+    color: "#FFECC8",
+  },
+  {
+    id: "6",
+    subject: "economics",
+    name: "Astra Valence",
+    topic: "How Markets Control Everyday Life",
+    duration: 10,
+    color: "#C8FFDF",
+  },
+];
